8fe7071c60ed2e9c143c3b8ba139189efbde5531
ignisdesign <brent@ignisdesign.com>
2016-10-11 13:30:27 -1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,9 +5,9 @@ display.setStatusBar( display.HiddenStatusBar )  -- Removes status bar
 
 -- Removes bottom bar on Android 
 if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
-    native.setProperty( "androidSystemUiVisibility", "lowProfile" )
+	native.setProperty( "androidSystemUiVisibility", "lowProfile" )
 else
-    native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
+	native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
 end
 
 -- Do not use buffer for console messages
@@ -15,29 +15,29 @@ end
 
 if isSimulator then 
 
-    -- Show FPS
-    local visualMonitor = require( "com.ponywolf.visualMonitor" )
-    local visMon = visualMonitor:new()
-    visMon.isVisible = false
+	-- Show FPS
+	local visualMonitor = require( "com.ponywolf.visualMonitor" )
+	local visMon = visualMonitor:new()
+	visMon.isVisible = false
 
-    -- Show/hide Physics
-    local function debugKeys( event )
-        local phase = event.phase
-        local key = event.keyName
-        if phase == "up" then
-            if key == "p" then
-                physics.show = not physics.show
-                if physics.show then 
-                    physics.setDrawMode( "hybrid" ) 
-                else
-                    physics.setDrawMode( "normal" )  
-                end
-            elseif key == "f" then
-                visMon.isVisible = not visMon.isVisible 
-            end
-        end
-    end
-    Runtime:addEventListener( "key", debugKeys )
+	-- Show/hide Physics
+	local function debugKeys( event )
+		local phase = event.phase
+		local key = event.keyName
+		if phase == "up" then
+			if key == "p" then
+				physics.show = not physics.show
+				if physics.show then 
+					physics.setDrawMode( "hybrid" ) 
+				else
+					physics.setDrawMode( "normal" )  
+				end
+			elseif key == "f" then
+				visMon.isVisible = not visMon.isVisible 
+			end
+		end
+	end
+	Runtime:addEventListener( "key", debugKeys )
 end
 
 -- Go to game screen
