7cf12e8e13d7ceb871e02c2528368f330d759c9f
Michael Wilson <mwilson@ponywolf.com>
2016-11-16 08:46:07 -0700

    Better documentation of main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,21 @@
-local composer = require( "composer" )
-local isSimulator = "simulator" == system.getInfo( "environment" )
+--[[
 
-display.setStatusBar( display.HiddenStatusBar )  -- Removes status bar
+This is the main.lua file. It executes first and in this demo
+is sole purpose is to set some initial visual settings and
+then you execute our game or menu scene via composer.
+
+Composer is the official scene (screen) creation and management
+library in Corona SDK. This library provides developers with an
+easy way to create and transition between individual scenes.
+
+https://docs.coronalabs.com/api/library/composer/index.html 
+
+-- ]]
+
+local composer = require( "composer" )
+
+ -- Removes status bar on iOS
+display.setStatusBar( display.HiddenStatusBar ) 
 
 -- Removes bottom bar on Android 
 if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
@@ -10,17 +24,19 @@ else
 	native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
 end
 
--- Do not use buffer for console messages
---io.output():setvbuf("no")
+-- are we running on a simulator?
+local isSimulator = "simulator" == system.getInfo( "environment" )
 
+-- if we are load our visual monitor that let's a press of the "F"
+-- key show our frame rate and memory usage
 if isSimulator then 
-
-	-- Show FPS
+	
+	-- show FPS
 	local visualMonitor = require( "com.ponywolf.visualMonitor" )
 	local visMon = visualMonitor:new()
 	visMon.isVisible = false
 
-	-- Show/hide Physics
+	-- show/hide physics
 	local function debugKeys( event )
 		local phase = event.phase
 		local key = event.keyName
@@ -40,5 +56,5 @@ if isSimulator then
 	Runtime:addEventListener( "key", debugKeys )
 end
 
--- Go to game screen
-composer.gotoScene( "scene.menu", { params={} } )
+-- go to menu screen
+composer.gotoScene( "scene.menu", { params={ } } )
