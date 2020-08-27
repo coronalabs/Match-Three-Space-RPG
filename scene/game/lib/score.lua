bc3f94b3073a7d7446d22bc582e0a3e3955fcacd
Michael Wilson <mwilson@ponywolf.com>
2020-08-27 15:40:53 -0600

    Fixed font issue with score

--- scene/game/lib/score.lua
+++ scene/game/lib/score.lua
@@ -10,16 +10,16 @@ function M.new(options)
   options = options or {}
   local label = options.label or ""
   local x,y = options.x or 0, options.y or 0
-  local font = options.font or "scene/game/font/Dosis-Bold.ttf"
-  local size = options.size or 56 
-  local align = options.align or "right"  
+  local font = options.font or "scene/menu/font/Dosis-Bold.ttf"
+  local size = options.size or 56
+  local align = options.align or "right"
   local stroked = options.stroked or true
   local color = options.color or {1, 1, 1, 1}
   local width = options.width or 256
 
-  --  create display group to hold visuals 
+  --  create display group to hold visuals
 
-  local score 
+  local score
   local num = options.score or 0
   local textOptions = { x=x, y=y, text=label .. " " ..  num, width = width, font=font, fontSize=size, align=align }
 
@@ -27,12 +27,12 @@ function M.new(options)
   score.num = num
   score.target = num
 
-  score:setFillColor( unpack(color) )  
+  score:setFillColor( unpack(color) )
 
   function score:add(points)
     score.target = self.target + (points or 10)
     local function countUp()
-      local diff = math.ceil((score.target - score.num) / 12)      
+      local diff = math.ceil((score.target - score.num) / 12)
       score.num = score.num + diff
       if score.num > score.target then
         score.num = score.target
@@ -41,7 +41,7 @@ function M.new(options)
       end
       score.text = label .. " " .. (score.num or 0)
     end
-    if not score.timer then 
+    if not score.timer then
       score.timer = timer.performWithDelay(30, countUp, -1)
     end
   end
@@ -49,7 +49,7 @@ function M.new(options)
   function score:get() return score.target or 0 end
 
   function score:finalize()
-    -- on remove cleanup instance 
+    -- on remove cleanup instance
     if score and score.timer then timer.cancel(score.timer) end
   end
 
